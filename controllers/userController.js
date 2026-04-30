251d56b882e15067f536e6c58b432ee807a7578e
AdamAliWeb <adamstar080904@gmail.com>
2026-04-29 18:47:08 -0600

    Added the JWT token middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -71,9 +71,9 @@ const loginUser = asyncHandler(async (req, res) => {
 
 //@desc Get the current user
 //@route GET api/users/current
-//@access public
+//@access private
 const currentUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Current user information" });
+  res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, currentUser };
